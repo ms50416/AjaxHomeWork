1dbb27ece665ab32a9d3e9cb479f8049dac7a128
浩恩 <ms60416@GMAIL.COM>
2023-01-09 17:59:05 +0800

    第二個作業OK

--- AjaxHomeWork/Controllers/HomeController.cs
+++ AjaxHomeWork/Controllers/HomeController.cs
@@ -7,16 +7,23 @@ namespace AjaxHomeWork.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly DemoContext _db;
+        public HomeController(ILogger<HomeController> logger, DemoContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult Name()
+        {
+
+            return View();
+        }
+
 
         public IActionResult Privacy()
         {
---
+++ AjaxHomeWork/Controllers/apiController.cs
@@ -0,0 +1,28 @@
+﻿using AjaxHomeWork.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxHomeWork.Controllers
+{
+    public class apiController : Controller
+    {
+        private readonly DemoContext _db;
+        public apiController(DemoContext db)
+        {
+          
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+            public IActionResult name(string name)
+            {
+            if (name == null)
+                return Content($"請輸入姓名", "text/plain", System.Text.Encoding.UTF8);
+            var _name = _db.Members.FirstOrDefault(m => m.Name == name);
+            if (_name == null)
+            return Content($"{name}可以使用", "text/plain", System.Text.Encoding.UTF8);
+            return Content($"{name}已被註冊過","text/plain",System.Text.Encoding.UTF8);
+            }
+        }
+}
